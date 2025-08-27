261a0867dab7bec37b789e22ff81583b37ec1393
kailashcsk <kailashchandrashenoyk@gmail.com>
2025-08-26 20:02:20 -0700

    Initial Commit

---
+++ HeightMeasureAppTests/HeightMeasureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeightMeasureAppTests.swift
+//  HeightMeasureAppTests
+//
+//  Created by Kailash Chandra Shenoy on 8/26/25.
+//
+
+import Testing
+@testable import HeightMeasureApp
+
+struct HeightMeasureAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
